4cd79fda1da9de1a3a564f29e4e180b00a7d14d2
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2023-11-09 13:40:32 -0400

    Refactor package.json and tsconfig.json, remove .eslintrc and update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'r2j6mq3x',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
