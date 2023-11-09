75b682ebbc545319f0b862a685f9d57bb4fe3bba
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2023-11-09 13:32:57 -0400

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'r2j6mq3x',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'yamilet',
+
+  projectId: 'r2j6mq3x',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
