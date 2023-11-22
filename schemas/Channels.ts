45f8764c7ee75406214359ba8f98a42ad953e106
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2023-11-22 07:54:22 -0400

    Chagne

---
+++ schemas/Channels.ts
@@ -0,0 +1,32 @@
+export default {
+	name: 'channels',
+	type: 'document',
+	title: 'Channels',
+	fields: [
+		{
+			name: 'title',
+			type: 'string',
+			title: 'Title',
+		},
+		{
+			name: 'category',
+			type: 'string',
+			title: 'Category',
+		},
+		{
+			name: 'category',
+			type: 'string',
+			title: 'Category',
+		},
+		{
+			name: 'number',
+			type: 'string',
+			title: 'Number',
+		},
+		{
+			name: 'image',
+			type: 'string',
+			title: 'Image',
+		},
+	],
+}
